0fb445fd04ec18d54c48879c47e2aa9bdb1887bc
Adam <adam.nemeth.0315@gmail.com>
2020-11-23 14:49:52 +0100

    Javascript first steps

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,11 +1,11 @@
+
+'use strict';
+
 const statusDisplay = document.querySelector('.game-status');
-let cells = [
-    ['','',''],
-    ['','',''],
-    ['','',''],
-];
+
 const gameActive = true;
-const currentPlayer = 'X';
+const playerOne = 'X';
+const playerTwo = 'O';
 
 const winningMessage = () => `The ${currentPlayer} has won!`;
 
@@ -13,3 +13,18 @@ const endMessage = () => `Game over.`
 
 const CurrentPlayerTurnMessage = () => `It's ${currentPlayer}'s turn!` ; 
 
+let cells = [
+    ['','',''],
+    ['','',''],
+    ['','',''],
+];
+
+cells.forEach(cell => cell.addEventListener('click', handleCellClick));
+
+function handleCellClick(){
+    for (let i = 0; i < cells.length; i++){
+        for (let j=0; j < cells[i].length; j++){
+
+        };
+    };
+};
