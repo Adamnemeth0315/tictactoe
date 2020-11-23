6f48b1d0f64fcf8e9c6dc8b04f6f17c68bdadadc
Adam <adam.nemeth.0315@gmail.com>
2020-11-23 22:05:26 +0100

    Még a nyertes üzenet hiányzik

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,31 +1,73 @@
 
 'use strict';
 
-const statusDisplay = document.querySelector('.game-status');
-const cells = Array.from(document.querySelectorAll('[data-cell-index]'));
-console.log(cells);
-const gameActive = true;
+const statusDisplay = document.querySelector('.game--status');
+const cells = document.querySelectorAll('[data-cell-index]');
+const startButton = document.querySelector('.startBtn');
+
+let matrix = ['', '', '', '', '', '', '', '','',];
+
 const playerOne = 'X';
 const playerTwo = 'O';
+const players = [playerOne, playerTwo];
+let winner =[];
+ 
+let clickNumber = 0;
 
-const winningMessage = () => `The ${playerOne} has won!`;
+startButton.addEventListener('click', function () {
+    clickNumber = 0;
+    cells.forEach(function (item, index) {
+        item.textContent = "";
+        matrix[index] = 0;
+    }) ;
+    statusDisplay.textContent = '';
+});
 
-const CurrentPlayerTurnMessage = () => `It's ${playerOne}'s turn!` ; 
+function game() {
+    clickNumber = 0;
+    for (let i = 0; i < cells.length; i += 1) {
+        cells[i].addEventListener('click', function () {
+            if (cells[i].textContent === '') {
+                if (clickNumber % 2 === 0) {
+                    cells[i].textContent = "X";
+                    matrix[i] = "X";
+                } else {
+                    cells[i].textContent = "O";
+                    matrix[i] = "O";
+                }
+                clickNumber ++; 
+                if (clickNumber > 4) {
+                    check();
+                }               
+            }
+        });
+    }
+    }
 
-let matrix = [
-    ['','',''],
-    ['','',''],
-    ['','',''],
-];
+function check() {
 
-function handleClick(){
-  for (let i = 0; i < cells.length; i++){
-                cells[i].addEventListener('click', handleCellClicked);
-}
+    winner = [
+        [matrix[0], matrix[1], matrix[2]],
+        [matrix[3], matrix[4], matrix[5]],
+        [matrix[6], matrix[7], matrix[8]],
+        [matrix[0], matrix[3], matrix[6]],
+        [matrix[1], matrix[4], matrix[7]],
+        [matrix[2], matrix[5], matrix[8]],
+        [matrix[0], matrix[4], matrix[8]],
+        [matrix[2], matrix[4], matrix[6]],
+    ]; 
+
+    for (let i =0; i < winner.length; i++){
+        if (winner[i] === 'X'){
+            statusDisplay.textContent = 'Az X játkos nyert';
+        } else if (winner[i] ==='O'){
+            statusDisplay.textContent = 'Az O játkos nyert';
+        } else if (clickNumber === 9) {
+            statusDisplay.textContent = 'Game over!';
+        } else {
+            clickNumber ++;
+        }
+    }        
 };
 
-function handleCellClicked (){
-    for (let i = 0; i < cells.length; i++){
-        cells[i].innerHTML = 'X';
-    }
-}
+game();
